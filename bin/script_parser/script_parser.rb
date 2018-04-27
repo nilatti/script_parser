76a85c37d2ac1bcd4b0761494446057b31d98e91
Alisha <nilatti@gmail.com>
2018-04-27 12:59:20 -0400

    optimized for gutenberg antony

--- bin/script_parser/script_parser.rb
+++ bin/script_parser/script_parser.rb
@@ -1,11 +1,14 @@
 require_relative '../../lib/script_parser'
 
 puts "ready to parse"
-@contents  = open_script("../../data/test_script.html")
+@contents  = open_script("./data/antony_and_cleopatra.html")
 get_characters
 edit_characters
 count_lines
 self.characters_sort
-@characters.each do |character|
-  puts "#{character.name}\t#{character.line_count}"
-end
+
+puts self.total_lines
+puts self.run_time_hours
+# @characters.each do |character|
+#   puts "#{character.name}\t#{character.line_count}"
+# end
--- lib/script_parser.rb
+++ lib/script_parser.rb
@@ -2,14 +2,27 @@
 #require 'open-uri'
 require_relative 'script_parser/character'
 @characters = Character.all_instances
-
-def list_characters
-	@characters = @characters.uniq { |p| p.name }
-	@characters.each do |character|
-		puts "#{character.id}  #{character.name}"
+def characters_count
+	@characters.size
+end
+def count_lines
+	@contents.each do |line|
+		unless line =~ /^\s*$/
+			if line =~ /^\s*([A-Z]+\s*[A-Z]*\s*\d*)\./
+					a = @characters.find { |x| x.name == $1 }
+				if a.class == Character
+					@current_character = a
+		    end
+			end
+	    if line =~ /Enter|Exeunt|Scene|Exit|^\s$/
+	    	@current_character = nil
+	    end
+	    unless @current_character.nil?
+				@current_character.increment_line_count
+			end
+		end
 	end
 end
-
 def edit_characters
     list_characters
     puts "If all characters look good, type \"Next\.\" Otherwise, type the ID number of the character you want to edit."
@@ -36,30 +49,10 @@ def edit_characters
         end
     end
 end
-
-def characters_count
-	@characters.size
-end
-def self.characters_sort
-	@characters.sort! { |a,b| a.line_count <=> b.line_count }
-end
-
-def open_script(file)
-	File.exists?(file)
-
-	script = open(file) #currently optimized for Duchess of Malfi from Project Gutenberg
-	contents = script.readlines
-	contents_br = []
-	contents.each do |c| #cleaning data up--default parser doesn't interpret <br> as new lines. Also apparently some encoding issues going on.
-		c.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
-		splits = c.split(/\<br\>/)
-		contents_br += splits
-	end
-	@contents = contents_br
-end
-
 def get_characters
+
 	dp = @contents.index { |x| x.include?("Dramatis Personae") } #where does the Dramatis Personae start?
+	puts dp
 	dp += 1 #we know we don't want the line that says "Dramatis Personae"
 
 	end_dp = @contents.index { |x| x.include?("ACT I")} #where does the Dramatis Personae end?
@@ -69,32 +62,43 @@ def get_characters
 
 	dramatis.each do |line|
 		if line =~ /([A-Z]{3,})/
-		  newbie = Character.new($1) #will need some edits, such as where Duchess script lists "BOSOLA" as BOSOLA throughout script but as "DANIEL DE BOSOLA" in the DP.
+			line.strip!
+		  newbie = Character.new(line) #will need some edits, such as where Duchess script lists "BOSOLA" as BOSOLA throughout script but as "DANIEL DE BOSOLA" in the DP.
 		end
 	end
 
 	puts "Created the following characters:"
 	list_characters
-
 end
-
-def count_lines
-	@contents.each do |line|
-		if line =~ /([A-Z]{3,})/
-	        a = @characters.find { |x| x.name == $1 }
-			if a.class == Character
-				@current_character = a
-	        end
-		end
-        if line =~ /\[|Enter|Exeunt|Scene|^\s$/
-        	@current_character = nil
-        end
-
-        unless @current_character.nil?
-			@current_character.increment_line_count
-		    puts @current_character.name
-		    puts line
-		end
+def list_characters
+	@characters = @characters.uniq { |p| p.name }
+	@characters.each do |character|
+		puts "#{character.id}  #{character.name}"
 	end
+end
+def open_script(file)
+	File.exists?(file)
 
+	script = open(file) #currently optimized for Duchess of Malfi from Project Gutenberg
+
+	contents = script.readlines
+	contents_br = []
+	contents.each do |c| #cleaning data up--default parser doesn't interpret <br> as new lines. Also apparently some encoding issues going on.
+		c.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+		splits = c.split(/\<br\>/)
+		contents_br += splits
+	end
+	@contents = contents_br
+end
+def self.run_time_hours
+	self.run_time_minutes/60
+end
+def self.run_time_minutes
+	self.total_lines/20
+end
+def self.total_lines
+	@characters.map(&:line_count).sum.to_f
+end
+def self.characters_sort
+	@characters.sort! { |a,b| a.line_count <=> b.line_count }
 end
